1d3e3d2b62ad08907abd8fb54972ce4a2f866fcf
Louis Irwin <coding@louisirwin.co.uk>
2020-03-10 23:38:06 +0000

    Minor bug fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import Buefy from "buefy";
 import App from "./App.vue";
 import store from "./store";
 import Moment from "moment";
-import router from "./router";
 
 import 'leaflet/dist/leaflet.css';
 
@@ -14,7 +13,6 @@ Vue.filter("formatDate", function(value) {
 });
 
 new Vue({
-  router,
   store,
   render: h => h(App)
 }).$mount("#app");
